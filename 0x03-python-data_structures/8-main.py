414ef0e6bb3cfcecaa60a5c6d931c5442836011f
shawnkiplagat69 <kiplagatshawn@gmail.com>
2022-12-04 17:55:28 +0300

    More returns!

---
+++ 0x03-python-data_structures/8-main.py
@@ -0,0 +1,4 @@
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("{:d} {:}".format(length, first))
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        j = None
+    j = sentence[0]
+    ans = (len(sentence), j)
+    return ans
