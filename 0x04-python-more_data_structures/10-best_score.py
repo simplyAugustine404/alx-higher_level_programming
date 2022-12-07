a836d8ea9477a8a326881eb6b8bb4dfb7091c720
shawnkiplagat69 <kiplagatshawn@gmail.com>
2022-12-07 10:42:19 -0500

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     val = max(list(a_dictionary.values()))
     for i, j in zip(a_dictionary.keys(), a_dictionary.values()):
         if j == val:
