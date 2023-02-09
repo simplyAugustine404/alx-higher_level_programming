85702efd0d7c7bd22f768f6127c4878c20534c6b
shawnkiplagat69 <kiplagatshawn@gmail.com>
2023-02-09 09:00:45 +0300

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Safe printing of a list"""
+
+
+def safe_print_list(my_list=[], x=0):
+    """Prints the members of a list"""
+
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print()
+    return ret
